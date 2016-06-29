e6075fd59bc1a9dc5b2e0d76fe831b3131b331d7
Ori Harel <ori.harel@gmail.com>
2016-06-30 00:17:20 +0300

    initial

---
+++ index.ios.js
@@ -0,0 +1,125 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity
+} from 'react-native';
+
+import {GoogleSignin, GoogleSigninButton} from 'react-native-google-signin';
+
+class NiyoRadar extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null
+    };
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount started...');
+    GoogleSignin.hasPlayServices({ autoResolve: true }).then(() => {
+
+      console.log('configuring GoogleSignin...');
+      GoogleSignin.configure({
+        scopes: ["https://www.googleapis.com/auth/drive.readonly"],
+        iosClientId: '586622831086-dte1gna5enr7dcfj0g4lkf6feh6h591t.apps.googleusercontent.com',
+        webClientId: '586622831086-uhmb85chi2i8jvnbqpqpnm80jqchelm0.apps.googleusercontent.com',
+        offlineAccess: false
+      });
+
+      console.log('figuring out current user...');
+      GoogleSignin.currentUserAsync().then((user) => {
+        console.log('USER', user);
+        this.setState({user: user});
+      }).done();
+
+    })
+    .catch((err) => {
+      console.log("Play services error", err.code, err.message);
+    })
+  }
+
+
+
+  render() {
+    if (!this.state.user) {
+      return (
+        <View style={styles.container}>
+          <GoogleSigninButton style={{width: 212, height: 48}} size={GoogleSigninButton.Size.Standard} color={GoogleSigninButton.Color.Light} onPress={this._signIn.bind(this)}/>
+        </View>
+      );
+    }
+
+    if (this.state.user) {
+      return (
+        <View style={styles.container}>
+          <Text style={{fontSize: 18, fontWeight: 'bold', marginBottom: 20}}>Welcome {this.state.user.name}</Text>
+          <Text>Your email is: {this.state.user.email}</Text>
+
+          <TouchableOpacity onPress={() => {this._signOut(); }}>
+            <View style={{marginTop: 50}}>
+              <Text>Log out</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      );
+    }
+
+  }
+
+  _signIn() {
+    console.log('_signIn started...');
+    try {
+      GoogleSignin.signIn()
+          .then((user) => {
+            console.log(user);
+            this.setState({user: user});
+          })
+          .catch((err) => {
+            console.log('WRONG SIGNIN', err);
+          })
+          .done();
+    }
+    catch (ex) {
+      console.error('Error trying to GoogleSignin.signIn() '+ex);
+    }
+
+  }
+
+  _signOut() {
+    GoogleSignin.revokeAccess().then(() => GoogleSignin.signOut()).then(() => {
+          this.setState({user: null});
+        })
+        .done();
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 30,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('NiyoRadar', () => NiyoRadar);
